01adc97c5fafd26a9e80404aca911f1dd26eab04
Dvir Volk <dvirsky@gmail.com>
2017-07-31 16:18:11 +0300

    added more options

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ func main() {
 
 	reader := flag.String("reader", "wiki", "Reader to use (wiki|reddit)")
 	path := flag.String("path", "./", "folder/file path")
+	cons := flag.Int("conns", 100, "Concuurent connections to redis")
+	files := flag.Int("rnum", 10, "Number of concurrent file readers")
+	host := flag.String("host", "localhost:6379", "Redis host")
+	index := flag.String("index", "idx", "Index name")
 
 	flag.Parse()
 	var sp indexer.SchemaProvider
@@ -19,21 +23,21 @@ func main() {
 
 	switch *reader {
 	case "wiki":
-		rd = indexer.NewFolderReader(*path, "*.xml", 8, indexer.DocumentReaderOpenerFunc(parser.WikiReaderOpen))
+		rd = indexer.NewFolderReader(*path, "*.xml", *files, indexer.DocumentReaderOpenerFunc(parser.WikiReaderOpen))
 		sp = indexer.SchemaProviderFunc(parser.WikipediaSchema)
 	case "reddit":
-		rd = indexer.NewFolderReader(*path, "*.bz2", 8, indexer.DocumentReaderOpenerFunc(parser.RedditReaderOpen))
+		rd = indexer.NewFolderReader(*path, "*.bz2", *files, indexer.DocumentReaderOpenerFunc(parser.RedditReaderOpen))
 		sp = indexer.SchemaProviderFunc(parser.RedditSchema)
 	default:
 		panic("Inavlid reader: " + *reader)
 	}
 
-	ch := make(chan redisearch.Document, 100)
+	ch := make(chan redisearch.Document, *cons)
 
 	if err := rd.Start(ch); err != nil {
 		panic(err)
 	}
 
-	idx := indexer.New("idx", "localhost:6379", 100, ch, nil, sp)
+	idx := indexer.New(*index, *host, *cons, ch, nil, sp)
 	idx.Start()
 }
