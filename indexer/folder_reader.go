4edc58b749c6905ee072f92828a357ae93439de9
Mark Nunberg <mnunberg@haskalah.org>
2017-08-03 12:59:18 -0700

    FolderReader: Close source channel when documents are exhausted

--- indexer/folder_reader.go
+++ indexer/folder_reader.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"github.com/RedisLabs/redisearch-go/redisearch"
 )
@@ -64,48 +65,77 @@ func (fr *FolderReader) walkDir(path string, pattern string, ch chan string) {
 	}
 }
 
+func (fr *FolderReader) readOne(f string, wg *sync.WaitGroup, waitch chan interface{}, ch chan<- redisearch.Document) (err error) {
+	// send something to the waitch that will be consumed by the workers
+	log.Println("Opening", f)
+	fp, err := os.Open(f)
+
+	if err != nil {
+		log.Println("Error opening ", f, ":", err)
+		return
+	}
+
+	go func(r io.Reader) {
+		// defer reading from the wait channel to signal that we've finished
+		defer func() {
+			wg.Done()
+			<-waitch
+		}()
+
+		dr, err := fr.opener.Open(fp)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		for err == nil {
+			doc, e := dr.Read()
+			if e == nil {
+				ch <- doc
+			} else {
+				log.Printf("Received error %s\n", e.Error())
+			}
+			err = e
+		}
+	}(fp)
+
+	return nil
+}
+
 func (fr *FolderReader) Start(ch chan<- redisearch.Document) error {
-	filech := make(chan string, fr.concurrency)
+	filech := make(chan string, 100)
+	waitch := make(chan interface{}, fr.concurrency)
+	donech := make(chan interface{}, 1)
 	go func() {
 		defer close(filech)
 		fr.walkDir(fr.folder, fr.pattern, filech)
 	}()
 
-	// start the independent idexing workers
+	wg := sync.WaitGroup{}
+
 	go func() {
-		waitch := make(chan struct{}, fr.concurrency)
 		for f := range filech {
-			// send something to the waitch that will be consumed by the workers
-			waitch <- struct{}{}
-			log.Println("Opening", f)
-			if fp, err := os.Open(f); err != nil {
-				log.Println("Error opening ", f, ":", err)
-			} else {
-				go func(r io.Reader) {
-					// defer reading from the wait channel to signal that we've finished
-					defer func() {
-						<-waitch
-					}()
-					dr, err := fr.opener.Open(fp)
-					if err != nil {
-						log.Println(err)
-						return
-					}
-					for err == nil {
-
-						doc, e := dr.Read()
-						if e == nil {
-							ch <- doc
-						}
-						err = e
-					}
-				}(fp)
-
+			if err := fr.readOne(f, &wg, waitch, ch); err == nil {
+				wg.Add(1)
+				// Add a slot to waitch, which is removed by the function we
+				// just called.
+				waitch <- nil
 			}
-
 		}
-
+		donech <- nil
 	}()
+
+	go func() {
+		// Wait until we know for sure that no more workers will be spawned
+		<-donech
+
+		// Wait for all the spawned workers to be done.
+		wg.Wait()
+
+		// And close the document channel once there are no more documents to write
+		close(ch)
+	}()
+
 	return nil
 }
 
